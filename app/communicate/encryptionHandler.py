389faffa7a9fcebd49ff3d49aacb4c3c915ed398
Max Stubenbord <max.stubi@t-online.de>
2019-12-04 14:22:55 +0100

    refactoring of encHandler with helperClasses.py

--- app/communicate/encryptionHandler.py
+++ app/communicate/encryptionHandler.py
@@ -1,5 +1,5 @@
-from diffiehellman.diffiehellman import DiffieHellman,MalformedPublicKey
-from cryptography.fernet import Fernet,InvalidToken
+from diffiehellman.diffiehellman import DiffieHellman, MalformedPublicKey
+from cryptography.fernet import Fernet, InvalidToken
 from hashlib import blake2b
 import json
 import sqlite3
@@ -7,16 +7,18 @@ import getFromDb
 import dataBaseConnection
 import jsonpickle
 import base64
+from helperClasses import DataBaseUtilities, UniversalUtilities
+
 
 class EncryptionHandler:
-    
+
     def createAndSaveSharedSecret(self, foreignPublicKey):
         '''Takes a public key from someone else and generates a sharedSecret'''
         keyHandler = self._getKeyHandlerFromDb()
         try:
             keyHandler.generate_shared_secret(foreignPublicKey)
-        except MalformedPublicKey:
-            print("ERROR: Could not create shared_secret. Please ensure a correct transfered public_key")
+        except MalformedPublicKey as e:
+            UniversalUtilities.sendErrorMessageToFrontend(e)
         self._insertKeyHandlerIntoDb(keyHandler)
 
     def encryptString(self, stringToEncrypt):
@@ -25,29 +27,31 @@ class EncryptionHandler:
         blake = blake2b(digest_size=16)
         try:
             blake.update(keyHandler.shared_key.encode())
-            fernet = Fernet(base64.urlsafe_b64encode(blake.hexdigest().encode()))
+            fernet = Fernet(base64.urlsafe_b64encode(
+                blake.hexdigest().encode()))
             # auf länge cappen (max 511 bytes) warten auf Kai
             return fernet.encrypt(json.dumps(stringToEncrypt).encode())
-        except AttributeError:
-            print("ERROR: Could not encrypt String. Please create a shared_secret with the createAndSaveSharedSecret() method beforehand")
+        except AttributeError as e:
+            UniversalUtilities.sendErrorMessageToFrontend(e)
             return None
-        except InvalidToken:
-            print("ERROR: Could not encrypt String. Please ensure a correct transfered public_key")
+        except InvalidToken as e:
+            UniversalUtilities.sendErrorMessageToFrontend(e)
             return None
-        
+
     def decryptByteArray(self, encryptedByteArray):
         '''Returns void if the decryption fails'''
         keyHandler = self._getKeyHandlerFromDb()
         blake = blake2b(digest_size=16)
         try:
             blake.update(keyHandler.shared_key.encode())
-            fernet = Fernet(base64.urlsafe_b64encode(blake.hexdigest().encode()))
+            fernet = Fernet(base64.urlsafe_b64encode(
+                blake.hexdigest().encode()))
             return json.loads(fernet.decrypt(encryptedByteArray))
-        except AttributeError:
-            print("ERROR: Please create a shared_secret with the createAndSaveSharedSecret() method beforehand")
+        except AttributeError as e:
+            UniversalUtilities.sendErrorMessageToFrontend(e)
             return None
-        except InvalidToken:
-            print("ERROR: Please ensure a correct transfered public_key")
+        except InvalidToken as e:
+            UniversalUtilities.sendErrorMessageToFrontend(e)
             return None
 
     def getLocalPublicKey(self):
@@ -64,42 +68,13 @@ class EncryptionHandler:
     def _insertKeyHandlerIntoDb(self, keyHandler):
         '''Encodes a DiffieHellman class as JSON and saves it as a string into settings table'''
         keyHandlerJson = jsonpickle.encode(keyHandler)
-        connection = dataBaseConnection.connectDb()
-        connection.row_factory = getFromDb.json_factory
-        cursor = connection.cursor()
         statement = '''INSERT OR REPLACE INTO settings (key, value) VALUES('keyHandlerJson', ?)'''
-        cursor.execute(statement, (keyHandlerJson,))
-        connection.commit()
-        connection.close()
+        DataBaseUtilities.insertIntoDb(statement, keyHandlerJson)
 
     def _getKeyHandlerFromDb(self):
         '''Fetches the DiffieHellman JSON from the settings table and returns the decoded class'''
-        connection = dataBaseConnection.connectDb()
-        connection.row_factory = getFromDb.json_factory
-        cursor = connection.cursor()
         statement = ''' SELECT value FROM settings WHERE key = 'keyHandlerJson' '''
-        cursor.execute(statement)
-        results = cursor.fetchall()
-
+        results = DataBaseUtilities.getValuesFromDb(statement)
         if len(results) == 0:
             return self._createAndSaveEncryptionKeys()
         return jsonpickle.decode(results[0]["value"])
-
-# DEBUGGING AND TESTING
-#
-#
-# keyHandler2 = DiffieHellman()
-# keyHandler2.generate_public_key()
-
-# encryptionHandler = EncryptionHandler()
-# # encryptionHandler.createAndSaveSharedSecret(keyHandler2.public_key)
-# message = { "message": "HEEEY WASSS UP HIER MEINE FINANZDATEN LG qwertzuioasdfghjk", "sender": "qwertzuioasdfghjk" }
-# encryptedM = encryptionHandler.encryptString(json.dumps(message))
-# messageRecovered = encryptionHandler.decryptByteArray(encryptedM)
-# print(messageRecovered)
-
-
-# encryptionHandler.createAndSaveSharedSecret(keyHandler2.public_key - 1) // raises MalformedPublicKey
-# fernet.decrypt raises InvalidToken
-
-# method getPublicKey()
