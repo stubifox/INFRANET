e0c6010826e92aae3e2f8f8a05bebb19e1b7f599
Kathun <kathun_c42@outlook.de>
2019-12-05 08:44:30 +0100

    Refactor for presentation

--- app/communicate/ArduinoConection.py
+++ app/communicate/ArduinoConection.py
@@ -4,39 +4,40 @@ import serial.tools.list_ports
 from threading import Thread
 
 # capsulates the communication to the Arduino using a Serial Com-Port connection
+
+
 class ArduinoConection:
 
-    def write(self,msg):
-        print("send message: {}".format(msg))
+    def write(self, msg):
         try:
             self.__serCon.write(msg)
         except serial.SerialTimeoutException:
-            print("arduino not reachable")
+            # TODO reset arduino connection
             self.__serCon = None
         except:
-            print("unkonw error occurd")
-        
-
+            # TODO logging
+            print("unknown error")
 
     def readline(self):
         msg = ""
         try:
-            msg = self.__serCon.readline()         
-            print("got ser message: {}".format(msg))    
+            msg = self.__serCon.readline()
         except serial.SerialTimeoutException:
-            print("arduino not reachable")
+            # TODO reset arduino connection
             self.__serCon = None
         except:
-            print("unkonw error occurd")
-        return msg  
+            # TODO Logging
+            print("unknown error")
+        return msg
 
     # returns wether a arduino ist connected to the host or not
     def getArdConState(self):
         # if no connection or error in connection
         if ((self.__serCon == None)):
-            # try to get a new connection 
+            # try to get a new connection
             if (self.SearchArdThread.is_alive() == False):
-                self.SearchArdThread = Thread(target=self.__getSerCon(),args=(self,))
+                self.SearchArdThread = Thread(
+                    target=self.__getSerCon(), args=(self,))
                 self.SearchArdThread.start()
             return False
         else:
@@ -48,53 +49,46 @@ class ArduinoConection:
         return self.__conToArd
 
     def __startComListener(self):
-        # TODO Implement
-        print("not jet implemented")        
+        # TODO Implement the listener if the arduino finds a com partner
+        raise NotImplementedError
 
     # searches all local ports for connected arduinos then checks if the respond correctly
     # returns the SerialConnection to the first correct responding arduino
     def __getSerCon(self):
-        print("start thread")
         self.__serCon = None
         AllPorts = list(serial.tools.list_ports.comports())
         for port in AllPorts:
-            # if not a arduino no testing required
+            # if not a arduino, no testing required
             if(str(port).__contains__("Arduino") == False):
                 continue
             # also tests if correct software on arduino
-            print("tried port{}".format(port.device))    
             try:
                 # Handshake Process
-                TestCon = serial.Serial(port=port.device,baudrate=115200,timeout=2,write_timeout=2)
-                time.sleep(2) # on each new connection the arduino will restart, waiting for it
-                TestCon.write(str.encode("~echo~\n"))
-                answer = TestCon.readline()
+                testCon = serial.Serial(
+                    port=port.device, baudrate=115200, timeout=2, write_timeout=2)
+                # on each new connection the arduino will restart, waiting for it
+                time.sleep(2)
+                testCon.write(str.encode("~echo~\n"))
+                answer = testCon.readline()
                 if (answer == str.encode("~ping~\n")):
-                    print("searching successfull")
                     # searching successfull
                     # set guid
-                    # wait for response -> check if guid was correctly recieve
-                    print("send guid")
-                    print(str.encode(str(self.__localId) + "\n"))
-                    TestCon.write(str.encode(str(self.__localId) + "\n"))
-                    print("wait for answer")
-                    answer = TestCon.readline()
-                    print("got answer")
-                    print(answer)
+                    # wait for response -> check if guid was correctly recieved
+                    testCon.write(str.encode(str(self.__localId) + "\n"))
+                    answer = testCon.readline()
                     if (answer != str.encode("~okay~\n")):
-                        print("Handshake failed")
-                        # handshake failed
+                        # TODO log handshake failed
+                        print("handshake failed")
                         self.__serCon = None
                         continue
                     else:
-                        #handshake successfull
-                        self.__serCon = TestCon
-                        print("Handshake success")
+                        # handshake successfull
+                        self.__serCon = testCon
                         break
             except:
                 # TODO Log Fail
                 print("error connecting to serialport: {}".format(port.device))
-        #return FoundConnection
+        # return FoundConnection
 
     def __init__(self, localId):
         self.__localId = localId
